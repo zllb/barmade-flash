33b5dc2c256aacce1e18822a8122ffe1e5bd0c72
Mitchell Hamann <captain@voyager.local>
2017-04-29 10:45:31 -0500

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'ingredients', component: IngredientsComponent},
+  {path: 'recipes', component: RecipesComponent},
+
 ];
 
 @NgModule({
