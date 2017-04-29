eb46935f459ccefb2d727cdee5eb52527faa13ad
Alec Hedlund <aleched@gmail.com>
2017-04-29 09:10:45 -0500

    adding bootstrap and ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AccordionModule } from 'ngx-bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { PaginationModule } from 'ngx-bootstrap';
+import { ProgressbarModule } from 'ngx-bootstrap';
+import { RatingModule } from 'ngx-bootstrap';
+import { SortableModule } from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap';
+import { TypeaheadModule } from 'ngx-bootstrap';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +22,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AccordionModule.forRoot(),
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    PaginationModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    RatingModule.forRoot(),
+    SortableModule.forRoot(),
+    TabsModule.forRoot(),
+    TypeaheadModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
